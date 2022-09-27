8158b1d91c0410cd3c9a2184504800649226bfee
junji oishi <abdaqoopmhm@gmail.com>
2022-09-27 23:07:34 +0900

    react-test-application
    CSSのテスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { useState, useCallback } from 'react';
-import { ChildArea } from './ChildArea';
+import { CssModules } from './components/CssModules';
+import { Emotion } from './components/Emotion';
+import { InlineStyle } from './components/InlineStyle';
+import { StyledComponent } from './components/StyledComponents';
+import { StyledJsx } from './components/StyledJsx';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  }
-  const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
   return (
     <>
-      <div>
-        <p>{count}</p>
-        <button onClick={onClickCountUp}>カウントアップ</button>
-      </div>
-      <div>
-        <input value={text} onChange={onChangeText} />
-        <br />
-        <br />
-        <button onClick={onClickOpen}>表示</button>
-        <ChildArea open={open} onClickClose={onClickClose}/>
-      </div>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponent />
+      <Emotion />
     </>
 
   );
